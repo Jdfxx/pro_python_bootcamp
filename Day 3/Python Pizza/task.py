9c1a0cbb0b192d93cfc4f99d71f90ad2fc8c7026
Filip <fhagno@yahoo.com>
2026-01-20 08:04:47 +0100

    Day 1 and 2

--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -24,3 +24,24 @@ if extra_cheese == "Y":
     total += extra_cheese_cost
 
 print(f"Your final bill is: ${total}.")
+total = 0
+peperoni_cost = 0
+extra_cheese_cost = 1
+
+if size == "S":
+    total += 15
+    peperoni_cost = 2
+elif size == "M":
+    total += 20
+    peperoni_cost = 3
+elif size == "L":
+    total += 25
+    peperoni_cost = 4
+
+if pepperoni == "Y":
+    total += peperoni_cost
+
+if extra_cheese == "Y":
+    total += extra_cheese_cost
+
+print(f"Your final bill is: ${total}.")
